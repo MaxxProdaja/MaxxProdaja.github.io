653a7f56af362bd9fa274619bbba428c88bf8ca0
MaxxProdaja <mahirtahirovic1@gmail.com>
2020-01-24 02:17:04 +0100

    MaxxProdaja.github.io
    maxprodaja je gotova. srecno

--- homepage1.js
+++ homepage1.js
@@ -84,3 +84,32 @@ $(function() {
     })
     
   })
+
+  
+  function proveri111()
+  {var uzorakporuka= /.{10,500}[\d]{3}[/][\d]{3}[-][\d]{2}[-][\d]{2}$/;
+  var poruka = document.getElementById("mail").value;
+  var porukica = document.getElementById('mail');
+if(uzorakporuka.test(poruka))
+{ $("#mail").css({"border-color":"green"});
+  alert("The message is in a good format");
+  document.getElementById("link").href="mailto:mahirtahirovic@hotmail.com?Subject=Really%20nice%20work%20Maki";
+}
+else{
+  $("#mail").css({"border-color":"red"});
+alert("Please enter your phone number at the end of the message in the format xxx/xxx-xx-xx eg 063/333-33-33");}
+  }
+
+  function proveri11()
+  {var uzorakporuka= /.{10,500}[\d]{3}[/][\d]{3}[-][\d]{2}[-][\d]{2}$/;
+  var poruka = document.getElementById("mail").value;
+  var porukica = document.getElementById('mail');
+if(uzorakporuka.test(poruka))
+{ $("#mail").css({"border-color":"green"});
+  alert("poruka je dobra");
+  document.getElementById("link1").href="mailto:mahirtahirovic@hotmail.com?Subject=Svaka%20cast%20Maki";
+}
+else{
+  $("#mail").css({"border-color":"red"});
+alert("Molim vas unesite vas broj telefona na kraju poruke u formatu xxx/xxx-xx-xx npr 063/333-33-33");}
+  }
--- homepage2.js
+++ homepage2.js
@@ -10,9 +10,11 @@ function promeni2(obj)
 function validatePassword(){
 if(password.value != confirm_password.value) {
 confirm_password.setCustomValidity("Lozinke se ne poklapaju!");
+$("#confirm_password").css({"border-color":"red"});
 exit;
 } else {
 confirm_password.setCustomValidity('');
+$("#confirm_password").css({"border-color":"green"});
 exit;
 }
 }
@@ -25,21 +27,24 @@ function provera()
       var imePrez = document.getElementById("imeprz").value;
       var mejl = document.getElementById("email").value;
       var uzorakIme = /^[A-Z]{1,}[_]{1}[A-Z ]{1,}$/;
-      var uzorakmejl = /^[a-zA-Z]{1}.{1,}\w{1}@(hotmail|live|gmail|np.ac)(.com|.rs)$/; 
-      var uzorakporuka= /.{10,500}[\d\d\d\/\d\d\d-\d\d-\d\d]$/;
+      var uzorakmejl = /^[a-zA-Z]{1}.{1,}\w{1}@(hotmail|live|gmail|np.ac)(.com|.rs)$/;
       if(uzorakIme.test(imePrez))
-       { 
+       { $("#imeprz").css({"border-color":"green"});
+         $("#nadimak").css({"border-color":"green"});
       imePrez1.setCustomValidity('');
          if(uzorakmejl.test(mejl))
          {
           mejl1.setCustomValidity(''); 
+          $("#email").css({"border-color":"green"});
           exit;
          }              
          else
-         mejl1.setCustomValidity('E-mail adresa: mora da pocinje slovom, nakon toga mogu biti specijalni karakteri, cifre ili slova, ali pre znaka @ mora biti slovo ili cifra.Ostali deo je standardni kao za sve e-mail adrese');
+         {mejl1.setCustomValidity('E-mail adresa: mora da pocinje slovom, nakon toga mogu biti specijalni karakteri, cifre ili slova, ali pre znaka @ mora biti slovo ili cifra.Ostali deo je standardni kao za sve e-mail adrese');
+         $("#email").css({"border-color":"red"});}
        }
       else
-      imePrez1.setCustomValidity('MORA BITI U FORMATU | IME_PREZIME | (Prezime moze da sadrzi razmak)');
+      {imePrez1.setCustomValidity('MORA BITI U FORMATU | IME_PREZIME | (Prezime moze da sadrzi razmak)');
+      $("#imeprz").css({"border-color":"red"});}
   }
 imePrez.onkeyup= provera;
 mejl.onkeyup= provera;
